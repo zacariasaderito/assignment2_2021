ed562b2e81b64ba1de3a1d35dd389ba4f9f09b12
zacariasaderito <aderitozacarias123@hotmail.com>
2021-05-10 16:51:58 +0200

    Initial Commit: Maven and Junit 5 successfuly Setup

---
+++ src/main/java/za/ac/cput/App.java
@@ -0,0 +1,18 @@
+package za.ac.cput;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+
+    public static int add(int n1, int n2)
+    {
+        return n1 + n2;
+    }
+}
---
+++ src/test/java/za/ac/cput/AppTest.java
@@ -0,0 +1,14 @@
+package za.ac.cput;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class AppTest {
+
+    @Test
+    void add() {
+
+        assertEquals(4 ,App.add(3,1));
+    }
+}
