f118653eac9033339cd617340dc27ee4d14ab0de
zacariasaderito <aderitozacarias123@hotmail.com>
2021-05-16 12:11:43 +0200

    Testing fase

--- src/main/java/za/ac/cput/App.java
+++ src/main/java/za/ac/cput/App.java
@@ -6,18 +6,18 @@ import java.util.*;
  * Hello world!
  *
  */
+
 public class App 
 {
 
-    private static Map m;
+    private static Map<Integer, String> m;
     private static Collection c;
     private static List l;
-    private static Set s;
+    private static Set<String> s;
 
-    public static void main( String[] args )
-    {
-//        list();
-        map();
+    public static void main( String[] args ) {
+        list();
+//        map();
 //        set();
 //        collection();
     }
@@ -31,11 +31,12 @@ public class App
         l.add("Object 3");
         l.add("Object 4");
         l.add("Object 5");
+        l.add("Object 5"); // duplicate
 
         // Removing from the List
         l.remove(3);
 
-        // Getting object from the List
+        // Finding object from the List
         String getObject = (String) l.get(3);
         System.out.println("The object you are looking for is: " + getObject + "\n");
 
@@ -52,6 +53,15 @@ public class App
 
     public static void collection() {
 
+        // Adding to a Collection
+
+        // Removing from the Collection
+
+        // Finding object from the Collection
+
+        // Iterate through the Collection
+
+        // Clear the Collection
     }
 
     public static void map() {
@@ -60,36 +70,59 @@ public class App
         System.out.println( "Testing!\n" );
 
         // Adding to the Map
-        m.put("1st", 0112);
-        m.put("2nd", 02212);
-        m.put("3rd", 0322);
-        m.put("4th", 04);
-        m.put("5th", 05412);
+        m.put(1, "1st Object");
+        m.put(2, "2nd Object");
+        m.put(3, "3rd Object");
+        m.put(4, "4th Object");
+        m.put(5, "5th Object");
 
         // Removing from the Map
+        m.remove(2);
 
-        // Getting object from the Map
+        // Finding object from the Map
+        String getObject = m.get(1);
+        String getObject1 = m.get(4);
 
         // Iterate through the Map
 
-        // Clear the Map
 
+        // Clear the Map
+        m.clear();
+
+        // display
+        System.out.println(getObject);
+        System.out.println(getObject1);
 
     }
 
     public static void set() {
 
+        s = new HashSet();
 
         // Adding to the Set
+        boolean added1 = s.add("Obj 1");
+        boolean added2 = s.add("Obj 2");
+        boolean added3 = s.add("Obj 3");
+        boolean added4 = s.add("Obj 3"); // Duplicate
 
         // Removing from the Set
+        boolean removed = s.remove("Obj 3");
+        boolean removed1 = s.remove("Obj 4"); // Unknown object
 
-        // Getting object from the Set
+        // Finding object from the Set
+        boolean isThere =  s.contains("Obj 1");
+
 
         // Iterate through the Set
+        for (String element: s) {
+            System.out.println(element);
+        }
 
         // Clear the Set
+        s.clear();
 
+        System.out.println(isThere);
 
     }
+
 }
--- src/test/java/za/ac/cput/AppTest.java
+++ src/test/java/za/ac/cput/AppTest.java
@@ -2,9 +2,113 @@ package za.ac.cput;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.*;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class AppTest {
 
 
+    @Test
+    void main() {
+    }
+
+    @Test
+    void list() {
+        List<String> l = new ArrayList();
+
+        // Adding to the List
+        l.add("Object 1");
+        l.add("Object 2");
+        l.add("Object 3");
+        l.add("Object 4");
+        l.add("Object 5");
+        l.add("Object 5"); // duplicate
+
+        assertFalse(l.isEmpty());
+        assertEquals("Object 3",l.get(2));
+        assertEquals(l.get(4),l.get(5)); // Check duplicate value
+        assertEquals(6,l.size());
+
+
+        // Removing from the List
+        l.remove(2);
+
+        assertEquals("Object 4",l.get(2));
+        assertEquals(5,l.size());
+
+
+        // Clear the List
+        l.clear();
+
+        assertEquals(0,l.size());
+        assertTrue(l.isEmpty());
+    }
+
+    @Test
+    void collection() {
+    }
+
+    @Test
+    void map() {
+
+        Map<Integer,String> m = new HashMap<>();
+        System.out.println( "Testing!\n" );
+
+        // Adding to the Map
+        m.put(1, "1st Object");
+        m.put(2, "2nd Object");
+        m.put(3, "3rd Object");
+        m.put(4, "4th Object");
+        m.put(5, "5th Object");
+
+        // Removing from the Map
+        m.remove(2);
+
+        // Finding object from the Map
+        String getObject = m.get(1);
+        String getObject1 = m.get(4);
+
+        // Iterate through the Map
+
+
+        // Clear the Map
+        m.clear();
+
+        // display
+        System.out.println(getObject);
+        System.out.println(getObject1);
+
+    }
+
+    @Test
+    void set() {
+
+        Set<String> s = new HashSet();
+
+        // Adding to the Set
+        boolean added1 = s.add("Obj 1");
+        boolean added2 = s.add("Obj 2");
+        boolean added3 = s.add("Obj 3");
+        boolean added4 = s.add("Obj 3"); // Duplicate
+
+        // Removing from the Set
+        boolean removed = s.remove("Obj 3");
+        boolean removed1 = s.remove("Obj 4"); // Unknown object
+
+        // Finding object from the Set
+        boolean isThere =  s.contains("Obj 1");
+
+
+        // Iterate through the Set
+        for (String element: s) {
+            System.out.println(element);
+        }
+
+        // Clear the Set
+        s.clear();
+
+        System.out.println(isThere);
+
+    }
 }
